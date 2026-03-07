fdcb5ebef5fc1a2ae0a7652ba05757ecbdaf166d
Joao Miguel Costa <miguel-cds@hotmail.com>
2026-03-07 01:30:06 +0100

    Agregar información del desarrollador en el pie de página

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -3,6 +3,7 @@ import "./Footer.css";
 export const Footer = () => `
 <footer>
   <span>© 2026 Pintarest — Fotos de <a href="https://unsplash.com" target="_blank">Unsplash</a></span>
+  <span>Dev -- Joao Costa -- </span>
   <span id="footerQuery"></span>
 </footer>
 `;
