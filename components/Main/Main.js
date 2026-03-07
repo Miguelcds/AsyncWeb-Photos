b44fb9b91044823f6a2ce41c43e630bfae427ccd
Joao Miguel Costa <miguel-cds@hotmail.com>
2026-03-07 01:05:23 +0100

    Refactor Main.js y Home.js: simplificar estructura y agregar lógica de categorías

--- components/Main/Main.js
+++ components/Main/Main.js
@@ -1,11 +1,15 @@
 import "./Main.css";
 
+// Genera los 4 contenedores de página vacíos.
+// Cada página (Home, Explore, Favorites, About) inyecta
+// su propio HTML dentro cuando el router la activa.
 export const Main = () => `
-<section>
-<h2 id="resultMessage"></h2>
-<ul id="result"></ul>
-</section>
+  <div class="page" id="page-home"></div>
+  <div class="page" id="page-explore"></div>
+  <div class="page" id="page-favorites"></div>
+  <div class="page" id="page-about"></div>
 `;
+
 
 // const acces = `https://api.unsplash.com/search/photos?query=gatos&per_page=20&client_id=9ZTYaTHYYbWrLsJ1o6MagjlViJ1RfNNUKbVxJasOx7I`
 
